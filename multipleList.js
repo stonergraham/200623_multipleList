cfa881ea92c4002415fd77062a5589caf642de49
Graham stoner <stoner.graham@gmail.com>
2020-06-25 15:32:34 -0600

    updating with test variables and logs

--- multipleList.js
+++ multipleList.js
@@ -1,5 +1,10 @@
 //Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num up to length:
 
+test1 = 17
+lnth1 = 6
+test2 = 9
+lnth2 = 19
+
 var multipleList = function(num, length) {
     var result = [];
     for (let i = 1; i < length + 1; i++) {
@@ -7,4 +12,5 @@ var multipleList = function(num, length) {
     }
     return result;
   };
-  console.log(multipleList(17, 6));
+  console.log(multipleList(test1, lnth1));
+  console.log(multipleList(test2, lnth2));
